1e51ca05175cf13b60e33d1d0000e039b1d44643
kbaba1001 <kbaba1001@gmail.com>
2014-11-09 00:19:36 +0900

    Setting DatabaseRewinder

--- features/support/env.rb
+++ features/support/env.rb
@@ -30,6 +30,7 @@ ActionController::Base.allow_rescue = false
 
 # Remove/comment out the lines below if your app doesn't have a database.
 # For some databases (like MongoDB and CouchDB) you may need to use :truncation instead.
+DatabaseCleaner = DatabaseRewinder
 begin
   DatabaseCleaner.strategy = :transaction
 rescue NameError
